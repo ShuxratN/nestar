fdcda43a7288136f9633402f18c26e0ae5e5b5c1
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-06-06 18:27:32 +0900

    feat: build project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver } from '@nestjs/apollo'
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { AppResolver } from './app.resolver';
       playground: true,
       uploads: false,
       autoSchemaFile: true,
-    }),
+    }), 
+    ComponentsModule,
    ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
