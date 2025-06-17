024cae4c31f05e0434ede7419fff003ee3655175
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-06-18 03:46:32 +0900

    feat: integration of Authentication and Authorisation

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -2,6 +2,10 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
 
 @Resolver()
 export class MemberResolver {
@@ -18,10 +22,21 @@ export class MemberResolver {
         console.log("Mutation: login");
         return this.memberService.login(input);
     }
-
+    @UseGuards(AuthGuard)
     @Mutation(() => String)
-    public async updateMember(): Promise<string> {
+    public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
         console.log("Mutation: updateMember");
+        console.log(typeof memberId);
+        console.log(memberId);
+        return this.memberService.updateMember();
+    }
+
+     @UseGuards(AuthGuard)
+    @Mutation(() => String)
+    public async checkAuth(@AuthMember('memberNick') memberNick: ObjectId): Promise<string> {
+        console.log("Query: checkAuth");
+        console.log("membeNick", memberNick);
+        console.log(memberNick);
         return this.memberService.updateMember();
     }
 
@@ -30,4 +45,19 @@ export class MemberResolver {
         console.log("Mutation: getMember");
         return this.memberService.getMember();
     }
+
+    // Admin //
+
+    // Authorization: Admin
+    @Mutation(() => String)
+    public async getAllMembersByAdmin(): Promise<string> {
+        return this.memberService.getAllMembersByAdmin();
+    }
+
+     @Mutation(() => String)
+    public async updateMemberByAdmin(): Promise<string> {
+        console.log("Mutation: updateMemberByAdmin");
+        return this.memberService.updateMemberByAdmin();
+    }
+
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -53,4 +53,12 @@ export class MemberService {
     public async getMember(): Promise<string> {
         return "getMember executed!";
     }
+
+    public async getAllMembersByAdmin(): Promise<string> {
+        return "getAllMemebersByAdmin executed!";
+    }
+
+    public async updateMemberByAdmin(): Promise<string> {
+        return "updateMemberByAdmin executed!"; 
+    }    
 }
