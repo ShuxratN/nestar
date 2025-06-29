d5ac114aa9a1ce7284034fda42e09293b0b22412
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-06-29 23:47:53 +0900

    feat: develop likeTargetBoardArticle  GraphQl APIs

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -60,6 +60,17 @@ export class BoardArticleResolver {
         return await this.boardArticleService.getBoardArticles(memberId, input);
     }
 
+    @UseGuards(AuthGuard)
+    @Mutation(() => BoardArticle)
+    public async likeTargetBoardArticle(
+    @Args('articleId') input: string,
+    @AuthMember('_id') memberId: ObjectId,
+    ) : Promise<BoardArticle> {
+    console.log("Mutation: likeTargetboardArticle");
+    const likeRefId = shapeIntoMongoObjectId(input);
+    return await this.boardArticleService.likeTargetBoardArticle(memberId, likeRefId)
+    }
+
     /**  Admin */
 
 
