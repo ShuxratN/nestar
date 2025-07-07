1ea7aa737678df196fe835eba1231f1e6fcfc1ea
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-07-07 18:41:21 +0900

    fix: modify batch module and connect to MongoDB

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,19 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'apps/nestar-api/src/app.module';
-import { LoggingInterceptor } from 'apps/nestar-api/src/libs/interceptor/Logging.interceptor';
-import { graphqlUploadExpress } from 'graphql-upload';
-import * as express from 'express';
+import { BatchModule } from './batch.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());
-  app.enableCors({ origin: true, credentials: true });
-
-  app.use(graphqlUploadExpress({ MaxFileSize: 15000000, maxFiles: 10 }));
-  app.use('/uploads', express.static('./uploads'));
-
+  const app = await NestFactory.create(BatchModule);
   await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
